7c36294c3255bf748ae38a9401608754e0a88bc9
fernanda <nanda_cbarros@hotmail.com>
2018-05-13 03:19:22 -0300

    adiciona ftp e smtp funcionando para cliente gmail com smtplib

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
 from socket import *
+import smtplib
+import getpass
 
 type = raw_input("HTTP, FTP ou SMTP?\n")
 while (not type.upper() in ['HTTP', 'FTP', 'SMTP']):
@@ -18,48 +20,75 @@ if type.upper() == "HTTP":
 	modifiedSentence = clientSocket.recv(1024)
 	print 'From Server:', modifiedSentence
 	clientSocket.close()
+
 if type.upper() == "FTP":
-	serverName = 'ftp.rediris.es'
+	serverName = 'ftp.cs.brown.edu'
 	serverPort = 21
 	clientSocket = socket(AF_INET, SOCK_STREAM)
 	clientSocket.connect((serverName,serverPort))
 	reply = ''
-	message = raw_input('Digite seu comando (para sair, q):\n')
-	while message != "q":
-		clientSocket.send(message + "\r\n")
 
-		# if(message == "LIST"):
-		# 	serverPort2 = 20
-		# 	dataSocket = socket(AF_INET, SOCK_STREAM)
-		# 	dataSocket.connect((serverName,serverPort2))
-		# 	reply = dataSocket.recv(65535)
-
-		reply = clientSocket.recv(65535)
-		# if not reply:
-		# 	break
-		# if '220 Only anonymous FTP is allowed here' in reply:
-		# 	clientSocket.send(message)
-		# 	break
-		print reply
-		message = raw_input('Digite seu comando (para sair, q):\n')
+	# Grab initial message
+	reply = clientSocket.recv(65535)
 	print reply
+	
+	# Anonymous login
+	login = "USER anonymous\r\n"
+	print("ftp> " + login)
+	clientSocket.send(login)
+	reply = clientSocket.recv(65535)
+	print reply
+
+	# Establish passive connection
+	message = "PASV\r\n"
+	print("ftp> " + message)
+	clientSocket.send(message)
+	reply = clientSocket.recv(65535)
+	print reply
+	ports = re.findall('[\d]+', reply)
+	# print(ports)
+	# Calculate passive port on server
+	data_port = int(ports[5]) * 256 + int(ports[6])
+	print("Data Port: " + str(data_port))
+	# Connect to PASV port
+	dataSocket = socket(AF_INET, SOCK_STREAM)
+	dataSocket.connect((serverName, data_port))
+
+	data_commands = ["LIST", "GET"]
+	
+	while True:
+		message = raw_input('ftp> ')
+		clientSocket.send(message + "\n")
+
+		if(message in data_commands):
+			reply = dataSocket.recv(65535)
+		elif message == "QUIT":
+			reply = clientSocket.recv(65535)
+			print reply
+			break
+		else:
+			reply = clientSocket.recv(65535)
+			
+		print reply
+
 if type.upper() == "SMTP":
-	email_from = raw_input('Digite seu email\n')
+	email_username = raw_input('Digite seu email\n')
+	email_password = getpass.getpass('Digite sua senha\n')
 	email_to = raw_input('Digite o email do destinatario\n')
 	email_subject = raw_input('Digite o assunto\n')
-	email_message = raw_input('Digite a mensagem\n')
-	serverName = "smtp.gmail.com"
-	serverPort = 465
-	clientSocket = socket(AF_INET, SOCK_STREAM)
-	clientSocket.connect((serverName,serverPort))
+	email_body = raw_input('Digite a mensagem\n')
 
-	print "CHEGOU AQUI"
-	message = raw_input("Digite sua msg para o servidor\n")
-	clientSocket.send(message + "\r\n")
+	#email_username = raw_input('testando.protocolo.smtp@gmail.com')
 
-	modifiedSentence = clientSocket.recv(1024)
-	print modifiedSentence
-	# print 'From Server:', modifiedSentence
-	clientSocket.close()
+	message = "From: " + email_username + "\r\n" + "To: " + email_to + "\r\n" + "Subject: " + email_subject + "\r\n" + email_body
 
+	# When an SMTP connection is secured via TLS/SSL, it is done over port 465
+	clientSocket = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+	clientSocket.ehlo()
 
+	clientSocket.login(email_username,email_password)
+	clientSocket.sendmail(email_username, email_to, message)
+
+	#modifiedSentence = clientSocket.recv(1024)
+	# print modifiedSentence
+	clientSocket.quit()
