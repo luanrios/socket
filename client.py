f142dc560fa13e4ca52082f06baa4b57d2eb4567
fernandabarros <nanda_cbarros@hotmail.com>
2018-05-24 19:10:53 -0300

    correções dns e melhoria socket http

--- client.py
+++ client.py
@@ -6,20 +6,33 @@ type = raw_input("HTTP, FTP ou SMTP?\n")
 while (not type.upper() in ['HTTP', 'FTP', 'SMTP']):
 	type = raw_input("Por favor, coloque um protocolo valido (HTTP, FTP ou STMP)\n")
 if type.upper() == "HTTP":
-	method = raw_input("Metodo (GET OU POST):\n")
-	host = raw_input("Nome do Hospedeiro:\n")
-	url = raw_input("Nome do Caminho:\n")
+	# PORTA 80
+	serverPort = raw_input("Digite a porta:\n")
+	serverName = raw_input("Digite o servidor:\n")
+	clientSocket = socket(AF_INET, SOCK_STREAM) 
+	clientSocket.connect((serverName,int(serverPort)))
+	requisicao1 = raw_input('Coloque sua requisicao GET/POST\n')
+	#"GET /index.html HTTP/1.1\r\n"
+	requisicao2 = raw_input('Coloque sua requisicao hostname\n')
+	requisicao1 = requisicao1 + "\r\n" + requisicao2 + "\r\n\r\n"
+
+	#requisicao + "Host: " + serverName + "\r\n\r\n"
+	# Host: www.serversmtp.com\r\n\r\n
+	clientSocket.send(requisicao1)
+	# Host: www.someschool.edu
+	serverResponse = clientSocket.recv(1024)
+	print serverResponse
+
+	clientSocket.close()
+"""
 	requisicao = method.upper() + " " + url + " " + "HTTP/1.1\r\n"
 	requisicao = requisicao + "Host: " + host + "\r\n\r\n"
 	serverName = host
-	serverPort = 80
-	clientSocket = socket(AF_INET, SOCK_STREAM) 
-	clientSocket.connect((serverName,serverPort))
 	print requisicao
 	clientSocket.send(requisicao)
 	modifiedSentence = clientSocket.recv(1024)
 	print 'From Server:', modifiedSentence
-	clientSocket.close()
+"""
 
 if type.upper() == "FTP":
 	serverName = 'ftp.cs.brown.edu'
@@ -74,10 +87,10 @@ if type.upper() == "FTP":
 if type.upper() == "SMTP":
 
 	smtp_server = raw_input('Digite o servidor SMTP\n')
-
+	print smtp_server
 	# Conexao SMTP segura (TLS/SSL) - porta:587 e inicia TLS
-	clientSocket = smtplib.SMTP(smtp_server, 587)
-	clientSocket.starttls()
+	print smtplib.SMTP(smtp_server, 587)
+	#clientSocket.starttls()
 
 	#Autenticacao e Login
 	email_username = raw_input('Digite seu email\n')
@@ -89,7 +102,7 @@ if type.upper() == "SMTP":
 	email_subject = raw_input('Digite o assunto\n')
 	email_body = raw_input('Digite a mensagem\n')
 
-	#Envia o email e encerra a conexão
+	#Envia o email e encerra a conexao
 	message = "From: " + email_username + "\r\n" + "To: " + email_to + "\r\n" + "Subject: " + email_subject + "\r\n\n" + email_body
 	clientSocket.sendmail(email_username, email_to, message)
 	clientSocket.quit()
