c607eb2be94f60d66a51fbf447e4db45cf0329e6
fernandabarros <nanda_cbarros@hotmail.com>
2018-05-28 00:07:51 -0300

    Delete api.py

--- api.py
+++
@@ -1,8 +0,0 @@
-type = raw_input("HTTP, FTP ou SMTP?\n")
-while (not type.upper() in ['HTTP', 'FTP', 'STMP']):
-	type = raw_input("Por favor, coloque um protocolo valido (HTTP, FTP ou STMP)\n")
-if type.upper() == "HTTP":
-	method = raw_input("Metodo (GET OU POST):\n")
-	url = raw_input("Endereco:\n")
-	requisicao = method + " " + url + " " + "HTTP/1.1"
-	
